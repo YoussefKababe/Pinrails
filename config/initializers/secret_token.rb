63c87d1e9ad344f4d32543e7c754e692c545a8f5
YoussefKababe <youssef.kbe@gmail.com>
2013-12-24 22:16:58 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pinrails::Application.config.secret_key_base = '0c96504edb30c300f64db0add836cba008ebec7b9e7c71fbaf88b1a43ed02cc4ac4479fb90dcde2f173467d6a8bd3968240d0e75c1a01ebb76268165e941a786'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pinrails::Application.config.session_store :cookie_store, key: '_pinrails_session'
